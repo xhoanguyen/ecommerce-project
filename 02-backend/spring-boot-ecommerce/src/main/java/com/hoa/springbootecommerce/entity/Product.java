707ea00e771b41868bd77280427cbb4873b76d55
xhoa <send2me2nd@gmail.com>
2022-10-26 23:07:11 +0200

    Added Product Entity

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/hoa/springbootecommerce/entity/Product.java
@@ -0,0 +1,58 @@
+package com.hoa.springbootecommerce.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name="Product")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "sku")
+    private String sku; // Stock keeping unit
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+    @Column(name = "image_url")
+    private String imageUrl;
+    @Column(name = "active")
+    private boolean active;
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
+    @Column(name = "last_updated")
+    @UpdateTimestamp
+    private Date lastUpdated;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Product product = (Product) o;
+        return id != null && Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
