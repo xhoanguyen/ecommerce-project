40b9ca1075ddcc0e69017d353faf4b8187598e23
xhoa <send2me2nd@gmail.com>
2022-11-05 00:47:47 +0100

    extend BackendService, searching for categoryId

--- 02-backend/spring-boot-ecommerce/src/main/java/com/hoa/springbootecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/hoa/springbootecommerce/dao/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.hoa.springbootecommerce.dao;
 
 import com.hoa.springbootecommerce.entity.Product;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
@@ -11,5 +15,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    // SPRING DATA REST automatically expose endpoints for query method
+    // and will execute a query similar to query method (i.e. findBy = SELECT * FROM...)
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 
 }
