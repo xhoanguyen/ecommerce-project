b28fc847ff1b499158d61104fe8baa0c3e6192cf
xhoa <send2me2nd@gmail.com>
2022-11-05 00:27:47 +0100

    SetUp Router Links to pass category id param

--- 03-frontend/angular-commerce/src/app/app.module.ts
+++ 03-frontend/angular-commerce/src/app/app.module.ts
@@ -5,11 +5,22 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
+import { Route, RouterModule, Routes } from '@angular/router';
 
-// TestCommment
+const routes: Routes = [
+  // When patch matches (category/:id) create new insance of component
+  // Most specifics fir, then generics
+  { path: 'category/:id', component: ProductListComponent },
+  { path: 'category/', component: ProductListComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '**', redirectTo: '/products', pathMatch: 'full' },
+];
+
 @NgModule({
   declarations: [AppComponent, ProductListComponent],
-  imports: [BrowserModule, HttpClientModule],
+  // Import all module for the frontend
+  imports: [RouterModule.forRoot(routes), BrowserModule, HttpClientModule],
   providers: [ProductService],
   bootstrap: [AppComponent],
 })
--- 03-frontend/angular-commerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-commerce/src/app/components/product-list/product-list.component.ts
@@ -1,24 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  // templateUrl: './product-list.component.html',
-  // templateUrl: './product-list-table.component.html',
   templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
-  constructor(private productService: ProductService) {}
+  currentCategoryId: number = 1;
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
   listProducts() {
-    this.productService.getProductList().subscribe((data) => {
-      this.products = data;
-    });
+    // check if "id" parameter is avaible
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // The '!' is the non-null assertion operator.
+      // This tells the compiler that hte object is not null
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }
+
+    this.currentCategoryId = 1;
+
+    this.productService
+      .getProductList(this.currentCategoryId)
+      .subscribe((data) => {
+        this.products = data;
+      });
   }
 }
